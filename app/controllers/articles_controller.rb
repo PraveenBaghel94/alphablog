49e68030fcefadb11c12f0dd750b8d791b8dabe2
Praveen Baghale <praveen.b@apextechnomatics.in>
2021-07-12 19:08:07 +0530

    DRY(DOnt Repeat Yourself code)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
-   
+    
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end  
     
     def index
@@ -13,11 +15,13 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end    
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        # @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
+
         # @article.save
         # # redirect_to article_path(@article)
         # redirect_to @article
@@ -30,9 +34,10 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
 
-        if @article.update(params.require(:article).permit(:title, :description))
+        # if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -45,4 +50,14 @@ class ArticlesController < ApplicationController
         @article.destroy
         redirect_to articles_path
     end    
+end    
+
+private
+
+def set_article
+    @article = Article.find(params[:id])
+end
+
+def article_params
+    params.require(:article).permit(:title, :description)
 end    
